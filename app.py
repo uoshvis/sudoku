7b2a667502ff1e4cd335ce09e1a659a144b9ccd7
uoshvis <uoshvis@gmail.com>
2020-09-24 14:14:11 +0300

    initial app window

---
+++ app.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+from settings import *
+
+
+class App:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.running = True
+
+    def run(self):
+        while self.running:
+            self.events()
+            self.update()
+            self.draw()
+        pygame.quit()
+        sys.exit()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.window.fill(WHITE)
+        pygame.display.update()
