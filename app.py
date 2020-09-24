4bd97e4d57e7f0935f29d2240e65afa5b7f6ab83
uoshvis <uoshvis@gmail.com>
2020-09-24 17:41:43 +0300

    add cell selection

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ class App:
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
         self.grid = testBoard
-        self.rows = 9
+        self.selected = None
+        self.mousePos = None
 
     def run(self):
         while self.running:
@@ -26,14 +27,34 @@ class App:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                selected = self.mouseOnGrid()
+                if selected:
+                    self.selected = selected
+                else:
+                    self.selected = None
 
     def update(self):
-        pass
+        self.mousePos = pygame.mouse.get_pos()
 
     def draw(self):
         self.window.fill(WHITE)
+        if self.selected:
+            self.drawSelection(self.window, self.selected)
         self.drawGrid(self.window)
         pygame.display.update()
+
+    def drawSelection(self, window, selected):
+        pygame.draw.rect(
+            window,
+            LIGHTBLUE,
+            (
+                (selected[0] * cellSize) + gridPos[0],
+                (selected[1] * cellSize) + gridPos[1],
+                cellSize,
+                cellSize
+            )
+        )
 
     def drawGrid(self, window):
         pygame.draw.rect(
@@ -43,7 +64,7 @@ class App:
             2
         )
 
-        for x in range(self.rows):
+        for x in range(ROWS):
             if x % 3 == 0:
                 thick = 2
             else:
@@ -64,3 +85,13 @@ class App:
                 (gridPos[0] + BOARD_WIDTH, gridPos[1] + (x * cellSize)),
                 thick
             )
+
+    def mouseOnGrid(self):
+        x_onBoard = gridPos[0] < self.mousePos[0] < gridPos[0] + BOARD_WIDTH
+        y_onBoard = gridPos[1] < self.mousePos[1] < gridPos[1] + BOARD_HEIGHT
+        if not x_onBoard or not y_onBoard:
+            return False
+        else:
+            row_x = (self.mousePos[0] - gridPos[0]) // cellSize
+            row_y = (self.mousePos[1] - gridPos[1]) // cellSize
+            return (row_x, row_y)
--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
 # Main window size
 WIDTH = 600
 HEIGHT = 600
+ROWS = 9
+
 
 # Colours
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+LIGHTBLUE = (100, 200, 230)
 
 # Boards
 
@@ -15,3 +18,4 @@ BOARD_WIDTH = 450
 BOARD_HEIGHT = 450
 gridPos = (75, 100)
 cellSize = 50
+GRIDSIZE = cellSize * ROWS
